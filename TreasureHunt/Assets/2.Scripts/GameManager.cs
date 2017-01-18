aea226b4791747d1f743d1b14b6fd1fd3e8a6fe1
dreamchaser3 <dreamchaser3@naver.com>
2017-01-18 22:09:40 +0900

    Initial Setting

---
+++ TreasureHunt/Assets/2.Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+
+            instance = this;
+
+        else if (instance != this)
+
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void MoveScene(string sceneName)
+    {
+        SceneManager.UnloadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
---
+++ TreasureHunt/Assets/2.Scripts/TouchCtrl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchCtrl : MonoBehaviour {
+    RaycastHit hit;
+    Ray ray;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.Log(ray);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(" you clicked on " + hit.collider.gameObject.name);
+
+            if (hit.collider.gameObject.tag == "TreasureBox")
+            {
+                hit.collider.gameObject.GetComponent<Animator>().SetTrigger("open");
+            }
+        }
+    }
+}

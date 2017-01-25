2f810fd9f617a3da1f19897cbf05b9700f858ceb
ss01363 <ss01363@naver.com>
2017-01-26 03:35:02 +0900

    Back Buttons on Views Also fixed some problems associated with panels: changed them into empty objects

---
+++ TreasureHunt/Assets/2.Scripts/Hunter/BackButtonController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BackButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Click(){
+		// scene #3 is Field View
+		SceneManager.LoadScene (3);
+	}
+
+}
+

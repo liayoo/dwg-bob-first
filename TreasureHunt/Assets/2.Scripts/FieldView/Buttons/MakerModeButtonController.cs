bef28743bc02fcd70822114fa5eb02779e315ee0
ss01363 <ss01363@naver.com>
2017-01-26 03:19:12 +0900

    Field View Buttons

---
+++ TreasureHunt/Assets/2.Scripts/FieldView/Buttons/MakerModeButtonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MakerModeButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Click(){
+		//Scene #6 is M_GameList View
+		SceneManager.LoadScene (6);
+	}
+}
---
+++ TreasureHunt/Assets/2.Scripts/FieldView/Buttons/MenuButtonController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuButtonController : MonoBehaviour {
+
+	bool flag = false;
+	public GameObject target;
+
+	Button myButton;
+
+	// Use this for initialization
+	void Awake(){
+		myButton = GetComponent<Button> ();
+		myButton.onClick.AddListener (Gotit);
+	}
+	void Start(){
+		target.SetActive (false);
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Gotit(){
+		if (flag) {
+			target.SetActive (false);
+			flag = false;
+		} else {
+			target.SetActive (true);
+			flag = true;
+		}
+	}
+}
+

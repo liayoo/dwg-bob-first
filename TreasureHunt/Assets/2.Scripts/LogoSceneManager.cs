c3a2871817eac4d28bc22ad030ab6764948ec2e5
dreamchaser3 <dreamchaser3@naver.com>
2017-01-27 03:14:59 +0900

    Add "Logo" Scene and some scripts for help

---
+++ TreasureHunt/Assets/2.Scripts/LogoSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(MoveNextScene());
+	}
+	
+	IEnumerator MoveNextScene()
+    {
+        yield return new WaitForSeconds(4f);
+        GameManager.instance.MoveScene("Login");
+    }
+}

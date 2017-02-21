395c1cc298c5d9143728fc5a86d7ceeada8eaa9c
dreamchaser3 <dreamchaser3@naver.com>
2017-02-22 05:02:10 +0900

    update

--- TreasureHunt/Assets/2.Scripts/FieldViewTouchCtrl.cs
+++ TreasureHunt/Assets/2.Scripts/FieldViewTouchCtrl.cs
@@ -21,13 +21,26 @@ public class FieldViewTouchCtrl : MonoBehaviour {
 				TreasureAttributes tr = hit.collider.gameObject.GetComponent<TreasureAttributes> ();
 				TreasureSetupController.currTargetName = tr.getTargetImg ();
                 GameManager.instance.treasure_id = int.Parse(tr.treasureID);
+                Debug.Log(GameManager.instance.treasure_id);
+
                 GameManager.instance.point = tr.treasurePoint;
-                GameManager.instance.minigameCat = tr.catchGameFlag;                
+                GameManager.instance.minigameCat = tr.catchGameFlag;
+                              
                 Debug.Log("The target image is" + TreasureSetupController.currTargetName);
-                if (count == 0)
+                if (GameManager.instance.minigameCat == 4)
                 {
-                    GameManager.instance.MoveScene("H_CatchView");
-                    count++;
+                    if (count == 0)
+                    {
+                        GameManager.instance.MoveScene("H_CatchView2D");
+                    }
+                }
+                else
+                {
+                    if (count == 0)
+                    {
+                        GameManager.instance.MoveScene("H_CatchView");
+                        count++;
+                    }
                 }
 			}
 		}
